8be1a82694fb2b1c027e994d3cc686d29e68a35a
Meraat <Meraat@gmail.com>
2022-10-19 22:34:46 +0330

    besmelah has been solved finally

--- src/components/PageContent.jsx
+++ src/components/PageContent.jsx
@@ -1,23 +1,24 @@
 import React from 'react'
 import { useState } from 'react'
 import { useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import { pages, suras } from '../quran-resources (farawin)/quran-metadata'
 import emla from '../quran-resources (farawin)/quran-text-emla'
-import Besmellah from './Besmellah'
-
 
 let PageContent = ({ page, id }) => {
 
   //variables
+  let cnt = 0
+  const [suraPages, setSuraPages] = useState(0)
   let arr = []
   const [pageTemp, setPageTemp] = useState([])
   const [myPage, setMyPage] = useState(page)
   let start = 0
   let end = 0
-  const [counter, setCounter] = useState([])
   const navigate = useNavigate()
   const [indexes, setIndexes] = useState([])
+  const [allStarts, setAllStarts] = useState([])
+
 
   //useEffects
   useEffect(() => {
@@ -31,19 +32,17 @@ let PageContent = ({ page, id }) => {
     }
   }, [myPage])
 
-  // useEffect(() => {
-  //   if (pageTemp.length > 0) {
-  //     addBesmellah(pageTemp, myPage)
-  //   }
-  // }, [myPage])
-
-  // useEffect(() => {
-  //   countAye(pageTemp, myPage)
-  // }, [pageTemp])
+  useEffect(() => {
+    suraPageCountForward()
+    if (suraPages === 1) {
+      navigate(`/sura/${parseInt(id) + 1}`)
+    }
+  }, [myPage])
 
   useEffect(() => {
-    findIndexes(pageTemp)
-  }, [pageTemp])
+    starts()
+  }, [])
+
 
   //functions
 
@@ -56,53 +55,48 @@ let PageContent = ({ page, id }) => {
     }
     setPageTemp(arr)
   }
-  // const addBesmellah = (pageTemp, myPage) => {
-  //   for (let c = 0; c < pageTemp.length; c++) {
-  //     for (let c1 = 1; c1 < suras.length; c1++) {
-  //       if (emla.indexOf(pageTemp[c]) === suras[c1][0] && c1 >= pages[myPage][0] && c1 <= pages[myPage + 1][0]) {
-  //         if (c1 !== c1 - 1 && c1 !== 9 && myPage !== 1) {
-  //           // console.log(suras[c1][4], c)
-  //           arr.splice(c, 0, 'بِسۡمِ اللّٰهِ الرَّحۡمٰنِ الرَّحٖیمِ')
-  //         }
-  //       }
-  //     }
-  //   }
-  // }
-  //====================
-  // const countAye = (pageTemp, myPage) => {
-  //   for (let c = 0; c < pageTemp.length; c++) {
-  //     for (let c1 = 1; c1 < suras.length; c1++) {
-  //       if (emla.indexOf(pageTemp[c]) < suras[c1][0]) {
-  //         // console.log(c1 - 1)
-  //         break
-  //       }
-  //     }
-  //   }
-  // }
   const nextPage = () => {
     setMyPage(myPage + 1)
   }
   const prePage = () => {
     setMyPage(myPage - 1)
   }
-  const findIndexes = (pageTemp) => {
-    let temp = []
-    for (let c = 0; c < pageTemp.length; c++) {
-      temp.push(emla.indexOf(pageTemp[c]))
+  const suraPageCountForward = () => {
+    for (let c = myPage; c < pages.length; c++) {
+      if (pages[c][0] !== parseInt(id)) {
+        break
+      }
+      cnt++
     }
-    setIndexes(temp)
-    for (let c1 = 1; c1 < suras.length; c1++) {
-
+    setSuraPages(cnt)
+  }
+  const starts = () => {
+    let arr = []
+    for (let c = 1; c < suras.length; c++) {
+      const sureDetail = {}
+      sureDetail.start = suras[c][0]
+      sureDetail.name = suras[c][4]
+      sureDetail.count = c
+      arr.push(sureDetail)
     }
+    setAllStarts(arr)
   }
   return (
     <div>
-      <h1>{myPage}</h1>
+      <h1>Page: {myPage}</h1>
+      <h2>Pages Left to end of this sura: {suraPages}</h2>
       {pageTemp.map((item, index) => {
-        return (<div key={index}>
-          {suras.filter((i, idx) => { return "بِسۡمِ اللّٰهِ الرَّحۡمٰنِ الرَّحٖیمِ" && i[0] === indexes[index] })}
-          {item}
-        </div>)
+        return (<h3 key={index}>
+          {allStarts.map((i, idx) => {
+            return (<div key={idx}>
+              {emla.indexOf(item) === i.start && <div>
+                <h1>{i.name}</h1>
+                {i.start !== 1235 && <h3> بِسۡمِ اللّٰهِ الرَّحۡمٰنِ الرَّحٖیمِ</h3>}
+              </div>}
+            </div>)
+          })}
+          <br />{item}
+        </h3>)
       })}
       <button onClick={nextPage}>next</button>
       <button onClick={prePage}>pre</button>
@@ -110,7 +104,7 @@ let PageContent = ({ page, id }) => {
       <button onClick={() => { navigate(`/sura/${parseInt(id) + 1}`) }}>Next Sure</button>
       <button onClick={() => { navigate(`/sura/${parseInt(id) - 1}`) }}>Pre Sure</button>
       <br />
-      <button onClick={() => { navigate('/') }}>Back to Menu </button>
+      <button onClick={() => { navigate('/') }}>Back to Menu</button>
     </div >
   )
 }
